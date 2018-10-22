5253f414a0904d10c53573b4d490889f5d0072e0
Paul Coroneos <paulalexandercoroneos@gmail.com>
2018-10-21 22:35:53 -0500

    compelte word.js. Test out word.js and letter.js

--- Letter.js
+++ Letter.js
@@ -1,20 +1,39 @@
+"use strict";
+
 class Letter {
-    constructor() {
-        this.character;
-        this.guessed;
-        this.letterGuessed = function (userLetter) {
-            if(userLetter.toLowerCase() === this.character) {
-                this.guessed = true;
-            }
-            else {
-                this.guessed = false;
-            }   
+    constructor(letter) {
+        this.character = letter;
+        this.guessed = false;
+    }
+    checkGuess(userLetter) {
+        if(userLetter.toLowerCase() === this.character) {
+            this.guessed = true;
+           // console.log(this.guessed);
         }
-        this.revealLetter = function () {
-            if(this.guessed)
-                return this.character;
-            else
-                return "_";
+        else {
+            this.guessed = false;   //redundant
+           // console.log(this.guessed);
+        }   
+    }
+
+    revealLetter() {
+        if(this.guessed) {
+           // console.log(this.character);
+            return this.character;
         }
+        else {
+           // console.log("_");
+            return "_";
+        }
+            
     }
 }
+
+//test code
+// let test = new Letter("a");
+
+// test.checkGuess("a");  
+// test.revealLetter();
+
+module.exports = Letter;
+    
--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+"use strict";
+
 // Load the NPM Package inquirer
-var inquirer = require("inquirer");
+let inquirer = require("inquirer");
+let Word = require('./Word.js');
 
 // Create a "Prompt" with a series of questions.
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Guess a letter!",
-      name: "letter-guess"
-    }
-  ])
-  .then(function(inquirerResponse) {
-    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+// inquirer
+//   .prompt([
+//     {
+//       type: "input",
+//       message: "Guess a letter!",
+//       name: "letter-guess"
+//     }
+//   ])
+//   .then(function(inquirerResponse) {
+//     // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
     
-  });
+//   });
+
+// test code for Letter.js
+  
+ let test = new Word("hello world");
